e20d513362552c0f934e7757a95422d3e465b3b4
Martin Cimerman <cim.martin@gmail.com>
2016-06-18 22:55:12 +0200

    added new app for word learning

---
+++ software/iOSApp/wordLearnApp/wordLearning/build/Pods.build/Debug-iphoneos/Pods-wordLearning.build/DerivedSources/Pods_wordLearning_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char Pods_wordLearningVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_wordLearning  PROJECT:Pods-1" "\n"; const double Pods_wordLearningVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ software/iOSApp/wordLearnApp/wordLearning/wordLearning-Bridging-file.h
@@ -0,0 +1,15 @@
+//
+//  wordLearning-Bridging-file.h
+//  
+//
+//  Created by Martin Cimerman on 17/06/16.
+//
+//
+
+#import <FGTranslator/FGTranslator.h>
+
+#ifndef wordLearning_Bridging_file_h
+#define wordLearning_Bridging_file_h
+
+
+#endif /* wordLearning_Bridging_file_h */
