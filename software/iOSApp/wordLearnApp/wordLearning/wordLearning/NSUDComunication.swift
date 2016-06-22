d4f6d7cf7bbf04343c71138a22dc3b7df23692cd
Martin Cimerman <cim.martin@gmail.com>
2016-06-22 23:50:18 +0200

    created interaction methods

--- software/iOSApp/wordLearnApp/wordLearning/wordLearning/NSUDComunication.swift
+++ software/iOSApp/wordLearnApp/wordLearning/wordLearning/NSUDComunication.swift
@@ -14,18 +14,78 @@ class NSUSerDefaultsComunication {
     let userDefaults = NSUserDefaults.standardUserDefaults()
     
     
+    func saveWordList(wordList: [Word]){
+        
+        let encodedList = self.encodeWordList(wordList)
+        self.saveArrayToNS(encodedList)
+        
+    }
+    
+    func loadWordList() -> [Word] {
+        
+        let encodedList = loadArrayFromNS()
+        let wordList = decodeWordList(encodedList)
+        return wordList
+        
+    }
     
     
-    func saveArrayToNS() -> Bool{
+    func encodeWordList(wordList: [Word]) -> [String] {
         
-        let array = ["",""]
+        var encodedArray = [String]()
         
-        userDefaults.setObject(array, forKey:"array")
+        for word in wordList {
+            
+            encodedArray += [word.inputWord,
+                             word.translatedWord,
+                             word.fromLanguage,
+                             word.toLanguage,
+                             word.imageReference]
+            
+        }
+        
+        return encodedArray
+        
+    }
+    
+    func decodeWordList(decodingArray: [String]) -> [Word]{
+        
+        
+        var wordList = [Word]()
+        let numberOfLoops = decodingArray.count/5
+        
+        for n in 0...numberOfLoops {
+            
+            let word = Word(inputWord: decodingArray[0+(n*5)],
+                            translatedWord: decodingArray[1+(n*5)],
+                            fromLanguage: decodingArray[2+(n*5)],
+                            toLanguage: decodingArray[3+(n*5)],
+                            imageReference: decodingArray[4+(n*5)])!
+            
+            wordList.append(word)
+            
+        }
+        
+        return wordList
+        
+    }
+    
+    
+    func loadArrayFromNS() -> [String] {
+        
+        var wordList = [String]()
+        wordList = userDefaults.objectForKey("wordListid") as! [String]
+        return wordList
+    }
+    
+    func saveArrayToNS(savingArray: [String]) {
+        
+        userDefaults.setObject(savingArray, forKey:"wordListid")
         
         userDefaults.synchronize()
 
         
-        return true
+        
     }
     
     
